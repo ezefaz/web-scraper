cfd1a05e074d66bf7612149d3f922d396ae751a7
efaszczak <efaszczak@envone.com>
2023-10-07 01:55:42 -0300

    update

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -53,3 +53,19 @@ export async function scrapeAndStoreProducts(productUrl: string) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
 }
+
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDb();
+
+        const product = await Product.findOne({ _id: productId })
+
+        if (!product) return;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
