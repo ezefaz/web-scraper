2428185e4bfdbbc9c1f18fcafdd8041d9b4f43ec
ezefaz <ezefaz@gmail.com>
2023-10-07 21:17:33 -0300

    update logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,51 @@
-import HeroCarousel from '@/components/HeroCarousel';
-import Searchbar from '@/components/Searchbar';
-import Image from 'next/image';
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
 
-import { getAllProducts } from '@/lib/actions';
-import ProductCard from '@/components/ProductCard';
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
 const Home = async () => {
-  const allProducts = await getAllProducts();
+	const allProducts = await getAllProducts();
 
-  return (
-    <>
-      {console.log('PANTERA', allProducts)}
-      <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
-        <div className='flex max-xl:flex-col gap-16'>
-          <div className='flex flex-col justify-center'>
-            <p className='small-text'>
-              Smart Shopping starts here:
-              <Image src='/assets/icons/arrow-right.svg' alt='arrow-right' width={16} height={16} />
-            </p>
-            <h1 className='head-text'>
-              Unleash the Power of <tr />
-              <span className='text-primary'>PriceWise</span>
-            </h1>
-            <p className='mt-6'>
-              Powerful, self-serve product and growth analytics to help you conver, engage and retain more.
-            </p>
-            <Searchbar />
-          </div>
-          <HeroCarousel />
-        </div>
-      </section>
-      <section className='trending-section'>
-        <h2 className='section-text'>Trending</h2>
-        <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {allProducts?.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className='px-6 md:px-20 py-24'>
+				<div className='flex max-xl:flex-col gap-16'>
+					<div className='flex flex-col justify-center'>
+						<p className='small-text'>
+							Smart Shopping starts here:
+							<Image
+								src='/assets/icons/arrow-right.svg'
+								alt='arrow-right'
+								width={16}
+								height={16}
+							/>
+						</p>
+						<h1 className='head-text'>
+							Descubre el potencial de <tr />
+							<span>Save</span>
+							<span className='text-primary'>Melin</span>
+						</h1>
+						<p className='mt-6'>
+							Potentes análisis de crecimiento y productos de autoservicio para
+							ayudarle a convertir, atraer y retener más.
+						</p>
+						<Searchbar />
+					</div>
+					<HeroCarousel />
+				</div>
+			</section>
+			<section className='trending-section'>
+				<h2 className='section-text'>Trending</h2>
+				<div className='flex flex-wrap gap-x-8 gap-y-16'>
+					{allProducts?.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Home;
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,96 +1,99 @@
-'use server'
+"use server";
 
 import axios from "axios";
-import * as cheerio from 'cheerio'
+import * as cheerio from "cheerio";
 import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
-
 export async function scrapeMLProduct(url: string) {
-    if (!url) return;    
+	if (!url) return;
 
-    // bright data proxy configuration
-    const username = String(process.env.BRIGHT_DATA_USERNAME)
-    const password = String(process.env.BRIGHT_DATA_PASSWORD)
-    const port = 22225
-    const session_id = (1000000 * Math.random()) | 0;
+	// bright data proxy configuration
+	const username = String(process.env.BRIGHT_DATA_USERNAME);
+	const password = String(process.env.BRIGHT_DATA_PASSWORD);
+	const port = 22225;
+	const session_id = (1000000 * Math.random()) | 0;
 
-    const options = {
-        auth: {
-            username: `${username}-session-${session_id}`,
-            password: password
-        },
-        host: 'brd.superproxy.io',
-        port,
-        rejectUnathorized: false,
-    }
+	const options = {
+		auth: {
+			username: `${username}-session-${session_id}`,
+			password: password,
+		},
+		host: "brd.superproxy.io",
+		port,
+		rejectUnathorized: false,
+	};
 
-    try {
+	try {
+		const response = await axios.get(url, options);
+		const $ = cheerio.load(response.data);
 
-    const response = await axios.get(url, options)
-    const $ = cheerio.load(response.data)
-     
-    const title = $('.ui-pdp-title').text().trim(); 
-    // const currentPrice = extractPrice(
-    //     $('.andes-money-amount__fraction'),
-    //     $('.andes-visually-hidden'),
-    //     $('.andes-money-amount__fraction'),
-    //     $('.ui-pdp-price__second-line'),
-    //     $('.ui-pdp-price__main-container'),
-    // );
-    const currentPrice: any = $('meta[itemprop="price"]').attr('content');
-    
-    const isOutOfStock = $('.ui-pdp-color--BLACK.ui-pdp-size--SMALL.ui-pdp-family--SEMIBOLD.ui-pdp-stock-information__title').text().trim().toLowerCase() !== 'stock disponible';  
-    
-    const images = 
-    $('.ui-pdp-image.ui-pdp-gallery__figure__image') ||
-    $('.ui-pdp-image');
+		const title = $(".ui-pdp-title").text().trim();
+		// const currentPrice = extractPrice(
+		//     $('.andes-money-amount__fraction'),
+		//     $('.andes-visually-hidden'),
+		//     $('.andes-money-amount__fraction'),
+		//     $('.ui-pdp-price__second-line'),
+		//     $('.ui-pdp-price__main-container'),
+		// );
+		const currentPrice: any = $('meta[itemprop="price"]').attr("content");
 
+		const isOutOfStock =
+			$(
+				".ui-pdp-color--BLACK.ui-pdp-size--SMALL.ui-pdp-family--SEMIBOLD.ui-pdp-stock-information__title"
+			)
+				.text()
+				.trim()
+				.toLowerCase() !== "stock disponible";
 
-    const imageUrls: string[] = []
-    
-    images.each((index, element) => {
-      const imageUrl = $(element).attr('src');
-      if (imageUrl) {
-        imageUrls.push(imageUrl);
-      }
-    });
+		const images =
+			$(".ui-pdp-image.ui-pdp-gallery__figure__image") || $(".ui-pdp-image");
 
-    const currency = extractCurrency($('.andes-money-amount__currency-symbol'))
+		const imageUrls: string[] = [];
 
-    const discountRate: any = $('.andes-money-amount__discount').text().replace(/[\s%]*OFF$/, '');
+		images.each((index, element) => {
+			const imageUrl = $(element).attr("src");
+			if (imageUrl) {
+				imageUrls.push(imageUrl);
+			}
+		});
 
-    const originalPrice = currentPrice ? (currentPrice * 100) / (100 - discountRate) : '';
+		const currency = extractCurrency($(".andes-money-amount__currency-symbol"));
 
-    const description = extractDescription($)
+		const discountRate: any = $(".andes-money-amount__discount")
+			.text()
+			.replace(/[\s%]*OFF$/, "");
 
-  // Construct data object with scraped information
+		const originalPrice = currentPrice
+			? (currentPrice * 100) / (100 - discountRate)
+			: "";
 
-  const data = {
-    url,
-    currency: currency || '$',
-    image: imageUrls[0],
-    title,
-    currentPrice: Number(currentPrice) || Number(originalPrice),
-    originalPrice: Number(originalPrice) || Number(currentPrice),
-    priceHistory: [],
-    discountRate: Number(discountRate),
-    category: 'category',
-    reviewsCount: 0,
-    stars: 4.5,
-    isOutOfStock,
-    description,
-    lowestPrice: Number(currentPrice) ||  Number(originalPrice),
-    highestPrice: Number(originalPrice) || Number(currentPrice),
-    averagePrice:  Number(currentPrice) ||  Number(originalPrice),
-  }
+		const description = extractDescription($);
 
-  console.log('DATA', data);
+		// Construct data object with scraped information
 
-  return data;
-  
-        
-    } catch (error: any) {
-        throw new Error(`Failed to scrape product; ${error.message}`)
-    }
+		const data = {
+			url,
+			currency: currency || "$",
+			image: imageUrls[0],
+			title,
+			currentPrice: Number(currentPrice) || Number(originalPrice),
+			originalPrice: Number(originalPrice) || Number(currentPrice),
+			priceHistory: [],
+			discountRate: Number(discountRate) || "",
+			category: "category",
+			reviewsCount: 0,
+			stars: 4.5,
+			isOutOfStock,
+			description,
+			lowestPrice: Number(currentPrice) || Number(originalPrice),
+			highestPrice: Number(originalPrice) || Number(currentPrice),
+			averagePrice: Number(currentPrice) || Number(originalPrice),
+		};
 
+		console.log("DATA", data);
+
+		return data;
+	} catch (error: any) {
+		throw new Error(`Failed to scrape product; ${error.message}`);
+	}
 }
