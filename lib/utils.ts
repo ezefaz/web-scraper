77ab66061b738fe306cf0508e610006084c09ba3
ezefaz <ezefaz@gmail.com>
2023-10-08 10:31:51 -0300

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,127 +1,126 @@
 import { PriceHistoryItem, Product } from "@/types";
 
 const Notification = {
-  WELCOME: 'WELCOME',
-  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
-  LOWEST_PRICE: 'LOWEST_PRICE',
-  THRESHOLD_MET: 'THRESHOLD_MET',
-}
+	WELCOME: "WELCOME",
+	CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+	LOWEST_PRICE: "LOWEST_PRICE",
+	THRESHOLD_MET: "THRESHOLD_MET",
+};
 
 const THRESHOLD_PERCENTAGE = 40;
 
 // Extracts and returns the price from a list of possible elements.
 export function extractPrice(...elements: any) {
-  for (const element of elements) {
-    const priceText = element.text().trim();
+	for (const element of elements) {
+		const priceText = element.text().trim();
 
-    if(priceText) {
-      const cleanPrice = priceText.replace(/[^\d.]/g, '');
+		if (priceText) {
+			const cleanPrice = priceText.replace(/[^\d.]/g, "");
 
-      let firstPrice; 
+			let firstPrice;
 
-      if (cleanPrice) {
-        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
-      } 
+			if (cleanPrice) {
+				firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+			}
 
-      return firstPrice || cleanPrice;
-    }
-  }
+			return firstPrice || cleanPrice;
+		}
+	}
 
-  return '';
+	return "";
 }
 
 // Extracts and returns the currency symbol from an element.
 export function extractCurrency(element: any) {
-  const currencyText = element.text().trim().slice(0, 1);
-  return currencyText ? currencyText : "";
+	const currencyText = element.text().trim().slice(0, 1);
+	return currencyText ? currencyText : "";
 }
 
 // Extracts description from two possible elements from amazon
 export function extractDescription($: any) {
-  // these are possible elements holding description of the product
-  const selectors = [
-    ".ui-pdp-description__content",
-    ".ui-pdp-description",
-    ".ui-pdp-description br",
-  ];
+	// these are possible elements holding description of the product
+	const selectors = [
+		".ui-pdp-description__content",
+		".ui-pdp-description",
+		".ui-pdp-description br",
+	];
 
-  for (const selector of selectors) {
-    const elements = $(selector);
-    if (elements.length > 0) {
-      const textContent = elements
-        .map((_: any, element: any) => $(element).text().trim())
-        .get()
-        .join("\n");
-      return textContent;
-    }
-  }
+	for (const selector of selectors) {
+		const elements = $(selector);
+		if (elements.length > 0) {
+			const textContent = elements
+				.map((_: any, element: any) => $(element).text().trim())
+				.get()
+				.join("\n");
+			return textContent;
+		}
+	}
 
-  // If no matching elements were found, return an empty string
-  return "";
+	// If no matching elements were found, return an empty string
+	return "";
 }
 
 export function getHighestPrice(priceList: PriceHistoryItem[]) {
-  console.log('TARARARARAA -->',priceList);
-  
-  let highestPrice = priceList[0];
+	console.log("TARARARARAA -->", priceList);
 
-  for (let i = 0; i < priceList.length; i++) {
-    if (priceList[i].price > highestPrice.price) {
-      highestPrice = priceList[i];
-    }
-  }
+	let highestPrice = priceList[0];
 
-  return highestPrice.price;
+	for (let i = 0; i < priceList.length; i++) {
+		if (priceList[i].price > highestPrice.price) {
+			highestPrice = priceList[i];
+		}
+	}
+
+	return highestPrice.price;
 }
 
 export function getLowestPrice(priceList: PriceHistoryItem[]) {
-  let lowestPrice = priceList[0];
+	let lowestPrice = priceList[0];
 
-  for (let i = 0; i < priceList.length; i++) {
-    if (priceList[i].price < lowestPrice.price) {
-      lowestPrice = priceList[i];
-    }
-  }
+	for (let i = 0; i < priceList.length; i++) {
+		if (priceList[i].price < lowestPrice.price) {
+			lowestPrice = priceList[i];
+		}
+	}
 
-  return lowestPrice.price;
+	return lowestPrice.price;
 }
 
 export function getAveragePrice(priceList: PriceHistoryItem[]) {
-  if (priceList.length === 0) {
-    return 0; // Handle the case when priceList is empty
-  }
-  
-  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
-  const averagePrice = sumOfPrices / priceList.length;
+	if (priceList.length === 0) {
+		return 0; // Handle the case when priceList is empty
+	}
 
-  return averagePrice;
+	const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+	const averagePrice = sumOfPrices / priceList.length;
+
+	return averagePrice;
 }
 
-
 export const getEmailNotifType = (
-  scrapedProduct: Product,
-  currentProduct: Product
+	scrapedProduct: Product,
+	currentProduct: Product
 ) => {
-  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+	const lowestPrice = getLowestPrice(currentProduct.priceHistory);
 
-  if (scrapedProduct.currentPrice < lowestPrice) {
-    return Notification.LOWEST_PRICE as keyof typeof Notification;
-  }
-  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
-    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
-  }
-  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
-    return Notification.THRESHOLD_MET as keyof typeof Notification;
-  }
+	if (scrapedProduct.currentPrice < lowestPrice) {
+		return Notification.LOWEST_PRICE as keyof typeof Notification;
+	}
+	if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+		return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+	}
+	if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+		return Notification.THRESHOLD_MET as keyof typeof Notification;
+	}
 
-  return null;
+	return null;
 };
 
 export const formatNumber = (num: number = 0) => {
-  return num.toLocaleString('es-AR', {
-    style: 'currency',
-    currency: 'ARS',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
+	return num.toLocaleString("es-AR", {
+		style: "currency",
+		currency: "ARS",
+		minimumFractionDigits: 0, // Set to the desired number of decimal places
+		maximumFractionDigits: 3,
+	});
 };
