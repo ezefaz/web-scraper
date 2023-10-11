2d5ea090f773acf7c70a66fffe956e407d3c9f37
efaszczak <efaszczak@envone.com>
2023-10-11 17:52:55 -0300

    update scraper

--- app/page.tsx
+++ app/page.tsx
@@ -10,22 +10,32 @@ const Home = async () => {
 
   return (
     <>
-      <section className='px-6 md:px-20 py-24'>
+      <section
+        className='px-6 md:px-20 py-24'
+        style={{
+          backgroundImage: "url('/assets/images/hero2.png')",
+          backgroundSize: 'cover',
+          height: '80vh',
+          backgroundPosition: 'center',
+          opacity: '70%',
+          backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust the alpha value (0.8) as needed
+        }}
+      >
         <div className='flex max-xl:flex-col gap-16'>
           <div className='m-auto'>
             <div className='small-text'>
               Empieza a ahorrar de manera gratuita y eficaz:
               <Image src='/assets/icons/arrow-right.svg' alt='arrow-right' width={16} height={16} />
             </div>
-            <h1 className='head-text'>Descubre el potencial de</h1>
+            <h1 className='head-text flex justify-center'>Descubre el potencial de</h1>
             {''}
             <div className='head-text'>
-              <div className='flex justify-center'>
+              <div className=''>
                 <span className=''>Save</span>
                 <span className='text-primary'>Melin</span>
               </div>
             </div>
-            <p className='mt-6'>
+            <p className='mt-6 text-gray-900'>
               A través de análisis y seguimiento de tus productos favoritos, te vamos a ayudar a ahorrar dinero a largo
               plazo.
             </p>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,54 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const navIcons = [
-	{ src: "/assets/icons/search.svg", alt: "Search" },
-	{ src: "/assets/icons/black-heart.svg", alt: "Heart" },
-	{ src: "/assets/icons/user.svg", alt: "User" },
+  { src: '/assets/icons/search.svg', alt: 'Search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'Heart' },
+  { src: '/assets/icons/user.svg', alt: 'User' },
 ];
 
 const Navbar = () => {
-	return (
-		<header className='w-full'>
-			<nav className='nav'>
-				<Link href='/' className='flex items-center gap-1'>
-					<Image
-						src='/assets/icons/savemelin3.svg'
-						width={150}
-						height={150}
-						alt='Logo'
-					/>
-				</Link>
-				<div className='flex items-center gap-5'>
-					{navIcons.map((icon) => (
-						<Image
-							src={icon.src}
-							alt={icon.alt}
-							width={28}
-							height={28}
-							className='object-contain'
-						/>
-					))}
-				</div>
-			</nav>
-		</header>
-	);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className='w-full'>
+      <nav className='bg-gray-200 p-4'>
+        <div className='container mx-auto flex justify-between items-center'>
+          <Link href='/' className='flex items-center gap-1'>
+            <Image src='/assets/icons/savemelin3.svg' width={120} height={100} alt='Logo' />
+          </Link>
+          <div className='lg:hidden flex items-center'>
+            <button className='text-2xl text-black hover:text-primary focus:outline-none' onClick={toggleMenu}>
+              ☰
+            </button>
+          </div>
+          <div className={`lg:flex items-center gap-5 text-black ${menuOpen ? 'block' : 'hidden'}`}>
+            <a href='#how-it-works' className='hover:underline'>
+              Funcionamiento
+            </a>
+            <a href='#pricing' className='hover:underline'>
+              Precios
+            </a>
+            <a href='#faqs' className='hover:underline'>
+              FAQs
+            </a>
+          </div>
+          <div className='flex items-center gap-5'>
+            {navIcons.map((icon) => (
+              <Image src={icon.src} alt={icon.alt} width={28} height={28} className='object-contain' />
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
