6aba5fb602a68a24fd8f884b773c23f6553935b4
efaszczak <efaszczak@envone.com>
2023-12-29 15:43:12 -0300

    fixing some bugs

--- app/api/webhook/user/route.ts
+++ app/api/webhook/user/route.ts
@@ -1,107 +1,95 @@
-import User from "@/lib/models/user.model";
-import { clerkClient } from "@clerk/nextjs/server";
-import { IncomingHttpHeaders } from "http";
-import { headers } from "next/headers";
-import { NextResponse } from "next/server";
-import { Webhook, WebhookRequiredHeaders } from "svix";
+import User from '@/lib/models/user.model';
+import { clerkClient } from '@clerk/nextjs/server';
+import { IncomingHttpHeaders } from 'http';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { Webhook, WebhookRequiredHeaders } from 'svix';
 
 interface EmailAddress {
-	email_address: string;
-	id: string;
-	linked_to: any[];
-	object: string;
-	reserved: boolean;
-	verification: {
-		[key: string]: any;
-	};
+  email_address: string;
+  id: string;
+  linked_to: any[];
+  object: string;
+  reserved: boolean;
+  verification: {
+    [key: string]: any;
+  };
 }
 
-interface Attributes {
-	id: string;
-	first_name: string;
-	profile_image_url: string;
-	email_addresses: EmailAddress[];
-	external_accounts: any[];
-}
-
-type EventType = "user.created" | "user.updated" | "*";
+type EventType = 'user.created' | 'user.updated' | '*';
 
 type Event = {
-	data: Record<string, string | number>;
-	object: "event";
-	type: EventType;
+  data: Record<string, string | number>;
+  object: 'event';
+  type: EventType;
 };
 
-const webhookSecret = process.env.WEBHOOK_SECRET || "";
+const webhookSecret = process.env.WEBHOOK_SECRET || '';
 
 async function handler(request: Request) {
-	const payload = await request.json();
-	const headersList = headers();
-	const heads = {
-		"svix-id": headersList.get("svix-id"),
-		"svix-timestamp": headersList.get("svix-timestamp"),
-		"svix-signature": headersList.get("svix-signature"),
-	};
-	const wh = new Webhook(webhookSecret);
-	let evt: any | null = null;
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    'svix-id': headersList.get('svix-id'),
+    'svix-timestamp': headersList.get('svix-timestamp'),
+    'svix-signature': headersList.get('svix-signature'),
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: any | null = null;
 
-	try {
-		evt = wh.verify(
-			JSON.stringify(payload),
-			heads as IncomingHttpHeaders & WebhookRequiredHeaders
-		) as Event;
-	} catch (err) {
-		console.error((err as Error).message);
-		return NextResponse.json({}, { status: 400 });
-	}
+  try {
+    evt = wh.verify(JSON.stringify(payload), heads as IncomingHttpHeaders & WebhookRequiredHeaders) as Event;
+  } catch (err) {
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
 
-	const eventType = evt.type;
-	if (eventType === "user.created" || eventType === "user.updated") {
-		const { id, ...attributes } = evt.data;
+  const eventType = evt.type;
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const { id, ...attributes } = evt.data;
 
-		try {
-			const existingUser = await User.findOne({
-				email: attributes.email_addresses[0]?.email_address,
-			});
+    try {
+      const existingUser = await User.findOne({
+        email: attributes.email_addresses[0]?.email_address,
+      });
 
-			if (existingUser) {
-				existingUser.name = attributes.first_name;
-				existingUser.email =
-					attributes.email_addresses[0]?.email_address || existingUser.email;
-				existingUser.image = attributes.profile_image_url || existingUser.image;
+      if (existingUser) {
+        existingUser.name = attributes.first_name;
+        existingUser.email = attributes.email_addresses[0]?.email_address || existingUser.email;
+        existingUser.image = attributes.profile_image_url || existingUser.image;
 
-				await existingUser.save();
+        await existingUser.save();
 
-				return NextResponse.json({
-					message: "El usuario fue actualizado con éxito.",
-					data: existingUser,
-				});
-			} else {
-				const newUser = new User({
-					name: attributes.first_name,
-					email: attributes.email_addresses[0]?.email_address,
-					image: attributes.profile_image_url || "",
-				});
+        return NextResponse.json({
+          message: 'El usuario fue actualizado con éxito.',
+          data: existingUser,
+        });
+      } else {
+        const newUser = new User({
+          name: attributes.first_name,
+          email: attributes.email_addresses[0]?.email_address,
+          image: attributes.profile_image_url || '',
+        });
 
-				await newUser.save();
+        await newUser.save();
 
-				return NextResponse.json({
-					message: "El usuario fue creado con éxito.",
-					data: newUser,
-				});
-			}
-		} catch (error) {
-			console.error("[WEBHOOK_USER]", error);
-			return NextResponse.json({
-				message: "Ha ocurrido un error al procesar la solicitud.",
-				data: null,
-			});
-		}
-	}
-	return NextResponse.json({
-		message: "Unhandled scenario or no action taken.",
-		data: null,
-	});
+        return NextResponse.json({
+          message: 'El usuario fue creado con éxito.',
+          data: newUser,
+        });
+      }
+    } catch (error) {
+      console.error('[WEBHOOK_USER]', error);
+      return NextResponse.json({
+        message: 'Ha ocurrido un error al procesar la solicitud.',
+        data: null,
+      });
+    }
+  }
+  return NextResponse.json({
+    message: 'Unhandled scenario or no action taken.',
+    data: null,
+  });
 }
 
 export const GET = handler;
--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,69 +1,69 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	// hashedPassword: {
-	//   type: String,
-	// },
-	image: {
-		type: String,
-	},
-	products: [
-		{
-			url: { type: String, unique: true },
-			currency: { type: String },
-			image: { type: String },
-			title: { type: String },
-			currentPrice: { type: Number },
-			originalPrice: { type: Number },
-			currentDolar: {
-				value: { type: Number },
-				date: { type: Date, default: Date.now },
-			},
-			priceHistory: [
-				{
-					price: { type: Number },
-					date: { type: Date, default: Date.now },
-				},
-			],
-			dolarHistory: [
-				{
-					value: { type: Number },
-					date: { type: Date, default: Date.now },
-				},
-			],
-			lowestPrice: { type: Number },
-			highestPrice: { type: Number },
-			averagePrice: { type: Number },
-			discountRate: { type: Number },
-			description: { type: String },
-			category: { type: String },
-			reviewsCount: { type: Number },
-			stockAvailable: { type: String },
-			stars: { type: String },
-			isOutOfStock: { type: Boolean, default: false },
-		},
-	],
-	// favoriteProducts: {
-	//   type: [String],
-	// },
-	subscription: {
-		type: String,
-		default: "basic",
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // hashedPassword: {
+  //   type: String,
+  // },
+  image: {
+    type: String,
+  },
+  products: [
+    {
+      url: { type: String, unique: true },
+      currency: { type: String },
+      image: { type: String },
+      title: { type: String },
+      currentPrice: { type: Number },
+      originalPrice: { type: Number },
+      currentDolar: {
+        value: { type: Number },
+        date: { type: Date, default: Date.now },
+      },
+      priceHistory: [
+        {
+          price: { type: Number },
+          date: { type: Date, default: Date.now },
+        },
+      ],
+      dolarHistory: [
+        {
+          value: { type: Number },
+          date: { type: Date, default: Date.now },
+        },
+      ],
+      lowestPrice: { type: Number },
+      highestPrice: { type: Number },
+      averagePrice: { type: Number },
+      discountRate: { type: Number },
+      description: { type: String },
+      category: { type: String },
+      reviewsCount: { type: Number },
+      stockAvailable: { type: String },
+      stars: { type: String },
+      isOutOfStock: { type: Boolean, default: false },
+    },
+  ],
+  favoriteProducts: {
+    type: [String],
+  },
+  subscription: {
+    type: String,
+    default: 'basic',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
