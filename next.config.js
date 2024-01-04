7c453d5ed48a8c76e3fe43f8efc210d728b7da99
efaszczak <efaszczak@envone.com>
2024-01-04 09:27:56 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true,
-		serverComponentsExternalPackages: ["mongoose"],
-	},
-	images: {
-		domains: [
-			"m.media-mercadolibre.com",
-			"http2.mlstatic.com",
-			"valordolarblue.com.ar",
-			"lh3.googleusercontent.com",
-		],
-	},
+  experimental: {
+    // serverActions: true,
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  images: {
+    domains: ['m.media-mercadolibre.com', 'http2.mlstatic.com', 'valordolarblue.com.ar', 'lh3.googleusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;
