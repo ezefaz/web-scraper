05bdafdc64cdbea254162631f9f2e89dffc94024
efaszczak <efaszczak@envone.com>
2024-02-10 20:27:56 -0300

    update dashboard

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -1,9 +1,16 @@
+import BusinessDashboard from "@/components/business/Dashboard";
+import Navbar from "@/components/business/NavBar";
 import React from "react";
 
 type Props = {};
 
 const BusinessPage = (props: Props) => {
-  return <div>BusinessPage</div>;
+  return (
+    <div className="mt-20">
+      <Navbar />
+      <BusinessDashboard />
+    </div>
+  );
 };
 
 export default BusinessPage;
